94ee4a05e5aac8b7e82377f40af53e9125e1ec7b
punky <punky@neva.zone>
2016-11-07 20:53:35 +0100

    Post app JS separated

---
+++ app/adm/src/main/resources/asset/post/app.js
@@ -0,0 +1,65 @@
+$(function () {
+    Handlebars.registerHelper('formatDate', function(date) {
+        return moment(date).format('MMMM Do YYYY, h:mm:ss');
+    });
+
+    var $list = $('.post-list');
+    var $form = $('.post-form');
+    var $script = $('script', $list);
+
+    var tpl = Handlebars.compile($script.html());
+    $script.remove();
+
+    function render() {
+        $.ajax({
+            method: 'GET',
+            url: $form.data('listUrl'),
+            success: function (posts) {
+                $list.html(tpl({posts: posts}));
+            },
+            error: function () {
+                alert('Cannot render posts properly due to internal server error.');
+            }
+        });
+    }
+
+    $form.on('submit', function () {
+        $.ajax({
+            url: $form.data('createUrl'),
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            type: 'POST',
+            processData: false,
+            success: function () {
+                render();
+            },
+            error: function () {
+                alert('Cannot add post due to internal server error.');
+            }
+        });
+
+        return false;
+    });
+
+    $list.delegate('.post-delete', 'click', function () {
+        var $link = $(this);
+
+        if (confirm("Are you sure want to delete this post?")) {
+            $.ajax({
+                method: 'DELETE',
+                url: $form.data('deleteUrl').split('<id>').join($link.data('id')),
+                success: function () {
+                    render();
+                },
+                error: function () {
+                    alert('Cannot delete post due to internal server error.');
+                }
+            });
+        }
+
+        return false;
+    });
+
+    render();
+});
