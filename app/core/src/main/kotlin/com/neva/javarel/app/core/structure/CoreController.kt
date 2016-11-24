eeb1a264033eac8d8ebabd725daea19ce46d7dbb
punky <punky@neva.zone>
2016-11-25 00:21:23 +0100

    App selector

--- app/core/src/main/kotlin/com/neva/javarel/app/core/structure/CoreController.kt
+++ app/core/src/main/kotlin/com/neva/javarel/app/core/structure/CoreController.kt
@@ -2,9 +2,7 @@ package com.neva.javarel.app.core.structure
 
 import com.neva.javarel.communication.rest.api.Redirect
 import com.neva.javarel.communication.rest.api.Route
-import com.neva.javarel.foundation.api.injection.Osgi
 import com.neva.javarel.framework.api.rest.Controller
-import com.neva.javarel.framework.api.structure.App
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.core.Response
@@ -12,14 +10,11 @@ import javax.ws.rs.core.Response
 @Path("/")
 class CoreController : Controller() {
 
-    @Osgi
-    private lateinit var app: App
-
     @Path("/")
     @GET
     @Route(names = arrayOf("home", "core.home"))
     fun getHome(): Response {
-        val route = app.modules.map { it.options.getOrElse("route", { "" }) as String }.first(String::isNotBlank)
+        val route = app.modules.map { (it.options["route"] ?: "") as String }.first(String::isNotBlank)
 
         return Redirect.to(urlGenerator.name(route))
     }
